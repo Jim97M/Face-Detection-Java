c752a707d3868ce30f1f5613f9f4080fa2fdcb1f
Jim97M <wabuyajames@gmail.com>
2020-09-10 13:57:12 +0300

    initial commit

---
+++ Constants.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JavaFaceApp;
+
+/**
+ *
+ * @author JIMMY
+ */
+public class Constants {
+    private Constants(){
+        
+    }
+    public static final String APPLICATION_NAME = "Face Detector";
+    public static final String EXIT_WARNING = "Do you want to exit";
+    public static final int   FRAME_WIDTH = 800;
+    public static final int IMAGE_LABEL_BORDER = 30;
+    public static final int FRAME_HEIGHT = 600;
+    public static final String CASCADE_CLASSIFIER = "";   
+    
+}
+foo
